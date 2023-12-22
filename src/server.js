d445a3abf0bf214f0bad3254c1212e5389c543a6
Alza Syauqi <syauqialza7@gmail.com>
2023-12-23 02:55:25 +0700

    add on-pre-response error in internal server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/notes-service');
 const NotesValidator = require('./validator/notes');
+const ClientError = require('./exceptions/client-error');
 
 const init = async () => {
   const noteService = new NotesService();
@@ -24,6 +25,24 @@ const init = async () => {
       service: noteService,
       validator: NotesValidator,
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    // get response context from request
+    const { response } = request;
+
+    // handling client errors internally
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
